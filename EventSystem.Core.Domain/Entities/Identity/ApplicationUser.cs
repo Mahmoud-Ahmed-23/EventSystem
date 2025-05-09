9c23861b0e70c16b55f87b696297a4a94f73aaa5
Mahmoud Ahmed <Mahmoud Ahmed@DESKTOP-3CFC79A>
2025-05-09 18:56:39 +0300

    add Identity Entities and thier configuration , DbContext and DbInitializer

---
+++ EventSystem.Core.Domain/Entities/Identity/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventSystem.Core.Domain.Entities.Identity
+{
+	public class ApplicationUser : IdentityUser
+	{
+		public DateTime CreatedAt { get; set; }
+		public required string FullName { get; set; }
+
+	}
+}
